b14294f6ead289ba2ca74f3cf86bf75273e6729a
MichalKs <mxiezopolski@gmail.com>
2014-12-03 21:37:19 +0100

    Copied IR files from STM32 (not divided into hal and app)

---
+++ AVR_IR/hal/inc/ir_hal.h
@@ -0,0 +1,23 @@
+/**
+ * @file: 	ir_hal.h
+ * @brief:	
+ * @date: 	3 gru 2014
+ * @author: Michal Ksiezopolski
+ *
+ *
+ * @verbatim
+ * Copyright (c) 2014 Michal Ksiezopolski.
+ * All rights reserved. This program and the
+ * accompanying materials are made available
+ * under the terms of the GNU Public License
+ * v3.0 which accompanies this distribution,
+ * and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * @endverbatim
+ */
+#ifndef HAL_INC_IR_HAL_H_
+#define HAL_INC_IR_HAL_H_
+
+
+
+#endif /* HAL_INC_IR_HAL_H_ */
